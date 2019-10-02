62452320c30d9e78e2b4623a5c423f1488375477
mikerspencer <spencer.mike.r@gmail.com>
2019-10-02 21:32:11 +0100

    fixed mean temp bug

--- forecast_run.R
+++ forecast_run.R
@@ -17,13 +17,15 @@ x = read_csv(f[1]) %>%
    gather(var, value, -observation_id, -year, -doy) %>% 
    separate(var, c("var", "model")) %>% 
    spread(var, value) %>% 
-   mutate(meantemp = mean(c(max, min))) %>% 
+   rowwise() %>% 
+   mutate(meantemp = (max + min) / 2) %>% 
    mutate(year = if_else(doy < 180, year - 1, year),
-          dowy = if_else(doy < 181, doy + 180, doy - 180)) %>% 
+          dowy = if_else(doy < 181, doy + 180, doy - 180),
+          Date = paste(year, dowy, sep = "_")) %>% 
    select(-min, -max, -doy, Precip = rainfall)
 
 
 # ---------------------------------------
 # Model run
 
-y = model(x)
+y = model(st = filter(x, model == 1 & year == 2019))
