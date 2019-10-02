d874bc93a543db886bd5053dc7fc0f90dd845b11
mikerspencer <spencer.mike.r@gmail.com>
2019-10-02 22:23:55 +0100

    rowise not needed

--- forecast_run.R
+++ forecast_run.R
@@ -5,34 +5,47 @@
 # ---------------------------------------
 
 library(tidyverse)
+library(parallel)
 source("model.R")
 
 
 # ---------------------------------------
-# Data in
+# files
 
 f = list.files("~/Documents/CNP_snow/data/forecast/Data", full.names = T)
 
-x = read_csv(f[1]) %>% 
-   gather(var, value, -observation_id, -year, -doy) %>% 
-   separate(var, c("var", "model")) %>% 
-   spread(var, value) %>% 
-   rowwise() %>% 
-   mutate(meantemp = (max + min) / 2) %>% 
-   mutate(year = if_else(doy < 180, year - 1, year),
-          dowy = if_else(doy < 181, doy + 180, doy - 180),
-          Date = paste(year, dowy, sep = "_")) %>% 
-   select(-min, -max, -doy, Precip = rainfall)
+
+# ---------------------------------------
+# Read, clean and run
+
+cg = lapply(f, function(i){
+   # Data in
+   x = read_csv(i) %>% 
+      gather(var, value, -observation_id, -year, -doy) %>% 
+      separate(var, c("var", "model")) %>% 
+      spread(var, value) %>% 
+      mutate(meantemp = (max + min) / 2) %>% 
+      mutate(year = if_else(doy < 180, year - 1, year),
+             dowy = if_else(doy < 181, doy + 180, doy - 180),
+             Date = paste(year, dowy, sep = "_")) %>% 
+      select(-min, -max, -doy, Precip = rainfall)
+   
+   z = mclapply(unique(x$model), mc.cores = 3, function(j){
+      # Model run
+      y = model(st = filter(x, model == j))
+      
+      # Days per year
+      y %>% 
+         group_by(year, observation_id, model) %>% 
+         summarise(M50 = sum(M50))
+   })
+   do.call("rbind.data.frame", z)
+})
 
 
 # ---------------------------------------
-# Model run
+# QA Plot
 
-y = model(st = filter(x, model == 1))
-
-y %>% 
-   group_by(year) %>% 
-   summarise(M50 = sum(M50)) %>% 
-   ggplot(aes(year, M50)) +
-   geom_point() +
-   stat_smooth()
+ggplot(z, aes(year, M50)) +
+   geom_point(aes(colour = model)) +
+   stat_smooth(SE = F)
